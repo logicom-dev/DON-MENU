d1de8a230450d39007acc97134b8d92fefadf02d
WafaZouari <88386735+WafaZouari@users.noreply.github.com>
2023-05-18 14:55:28 +0200

    backend update

--- BACKEND-DON/routes/articles.route.js
+++ BACKEND-DON/routes/articles.route.js
@@ -1,16 +1,33 @@
 const express = require('express');
+const { article } = require('../models/article');
+const db = require("../models/article");
+const Article = db.article;
 
 //import  upload from '../middleware/uploadProvider.js';
-const { getArticles, createArticle, deleteArticle, updateArticle, getArticleByID, deleteArticle2} = require('../controllers/articles');
+const { getArticles, createArticle, deleteArticle, updateArticle, getArticleByID, deleteArticle2, getArticles2} = require('../controllers/articles');
 const { uploadImg } = require("../middleware/multer")
 
 const router = express.Router();
-router.get('/', getArticles);
+
+const {verifyToken} =require("../middleware/verify-token")
+// afficher la liste des articles.
+router.get('/', verifyToken, async (req, res )=> {
+try {
+    const art = await db.sequelize.query(`select CodeArt, LibArt, Descrip, CodeCat, prix1, image_web, visible_web from article `);
+
+    res.status(200).json(art[0]);
+} catch (error) {
+res.status(404).json({ message: error.message });
+}
+}, getArticles);
+
+//router.get('/', getArticles);
+router.get('/articles', getArticles2);
 router.post('/', uploadImg.single("imagepath"), createArticle);
 router.get('/:CodeArt', getArticleByID);
 router.post('/', createArticle);
 router.put('/:CodeArt', uploadImg.single("imagepath"), updateArticle);
 //router.put('/articles/:CodeArt', deleteArticle);
-router.delete('/:CodeArt', deleteArticle2)
+router.delete('/:CodeArt', deleteArticle2);
 
 module.exports = router;
