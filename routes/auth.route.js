20b1b89f3b9a1795f991741d5c251b6f97c80d70
logicom-dev <121340419+logicom-dev@users.noreply.github.com>
2024-04-23 09:40:08 +0100

    Delete routes directory

--- routes/auth.route.js
+++
@@ -1,7 +0,0 @@
-const express= require('express');
-const router = express.Router();
-const { getAuth , createAdmin} =require('../controllers/auth');
-router.post('/auth', getAuth)
-router.post('/addAdmin', createAdmin)
-
-module.exports = router;
