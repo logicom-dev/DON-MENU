33bf93e5ce3568a226fe8b5b98162341f0648c7a
logicom-dev <121340419+logicom-dev@users.noreply.github.com>
2024-04-23 09:34:18 +0100

    Delete ADMIN directory

--- ADMIN/src/Axios/Api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-export default axios.create({
-    baseURL: process.env.REACT_APP_API
-})
-
-// export default axios.create({
-//     baseURL: "http://localhost:3000/api/"
-// "https://mascotte-api.onrender.com/api"
-// })
