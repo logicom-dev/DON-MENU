79487d85bf05156d96aceead6a0fcb5a72ba0042
logicom-dev <121340419+logicom-dev@users.noreply.github.com>
2024-04-23 09:35:51 +0100

    Delete BACKEND-DON directory

--- BACKEND-DON/routes/commande.route.js
+++
@@ -1,9 +0,0 @@
-const express= require('express');
-const router = express.Router();
-const {createCommande , getCommande} =require('../controllers/commande');
-
-
-router.get('/',getCommande);
-router.post('/' , createCommande );
-
-module.exports = router;
